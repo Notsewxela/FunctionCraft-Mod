b274bc52c418109b845053f053f91f241f358bfc
Notsewxela <notsewxela@aol.com>
2014-08-08 19:22:18 +0100

    Updated to 1.7.10

--- src/main/java/com/notsewxela/functioncraft/FunctionCraft.java
+++ src/main/java/com/notsewxela/functioncraft/FunctionCraft.java
@@ -1,38 +1,43 @@
 package com.notsewxela.functioncraft;
 
+import com.notsewxela.functioncraft.handler.ConfigurationHandler;
 import com.notsewxela.functioncraft.proxy.IProxy;
 import com.notsewxela.functioncraft.reference.Reference;
+import com.notsewxela.functioncraft.utility.LogHelper;
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, guiFactory = Reference.GUI_FACTORY_CLASS)
 public class FunctionCraft
 {
     @Mod.Instance(Reference.MOD_ID)
     public static FunctionCraft instance;
 
-    @SidedProxy(clientSide = "com.notsewxela.functioncraft.proxy.ClientProxy", serverSide = "com.notsewxela.functioncraft.proxy.ServerProxy")
+    @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
     public static IProxy proxy;
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
-
+        ConfigurationHandler.init(event.getSuggestedConfigurationFile());
+        FMLCommonHandler.instance().bus().register(new ConfigurationHandler());
+        LogHelper.info("Pre Initialisation Complete!");
     }
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event)
     {
-
+        LogHelper.info("Initialisation Complete!");
     }
 
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event)
     {
-
+        LogHelper.info("Post Initialisation Complete!");
     }
 
 }
---
+++ src/main/java/com/notsewxela/functioncraft/client/gui/GuiFactory.java
@@ -0,0 +1,37 @@
+package com.notsewxela.functioncraft.client.gui;
+
+import cpw.mods.fml.client.IModGuiFactory;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
+
+import java.util.Set;
+
+/**
+ * Created by Mark on 08/08/2014.
+ */
+public class GuiFactory implements IModGuiFactory
+{
+    @Override
+    public void initialize(Minecraft minecraftInstance)
+    {
+
+    }
+
+    @Override
+    public Class<? extends GuiScreen> mainConfigGuiClass()
+    {
+        return ModGuiConfig.class;
+    }
+
+    @Override
+    public Set<RuntimeOptionCategoryElement> runtimeGuiCategories()
+    {
+        return null;
+    }
+
+    @Override
+    public RuntimeOptionGuiHandler getHandlerFor(RuntimeOptionCategoryElement element)
+    {
+        return null;
+    }
+}

3d649e3562725993a6a8b41b478bc5e813264704
Notsewxela <notsewxela@aol.com>
2015-03-07 13:02:50 +0000

    Not nothing at all

--- src/main/java/com/notsewxela/functioncraft/item/ItemLeadIngot.java
+++ src/main/java/com/notsewxela/functioncraft/item/ItemLeadIngot.java
@@ -1,6 +1,9 @@
 package com.notsewxela.functioncraft.item;
 
 import com.notsewxela.functioncraft.creativetab.CreativeTabFC;
+import com.notsewxela.functioncraft.init.ModItems;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
 
 public class ItemLeadIngot extends ItemFunctionCraft
 {
@@ -10,5 +13,7 @@ public class ItemLeadIngot extends ItemFunctionCraft
         this.setUnlocalizedName("ingotLead");
         this.setMaxStackSize(64);
         this.setCreativeTab(CreativeTabFC.FC_TAB);
+        // THIS DOESN'T WORK HKTRNOKTRIBHIEUG: this.setPotionEffect("Potion.poison.id");
+
     }
 }
